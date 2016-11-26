642a881c4e6b2124fba3a4eae9417b785f3c49e5
Rishit Khare <AdarshKhare@Home-1s-MacBook-Pro.local>
2016-11-26 14:40:21 -0800

    create Functions
    Created functions and global variables.

--- Game.py
+++ Game.py
@@ -10,89 +10,134 @@ import random
 #     3. unexpected events (Fired from job, enemy attacks, robbery)
 #     4. Add Konami Code?
 
-#Event variables
-location = "B"
-event_list = ["were fired","were robbed", "were promoted", "won the lottery"]
-eventTiming = int(random.uniform(3,10))
-event = 0
-#Job variables
-job = True
-findJob = 0
-salary = 1
-#Inventory & Misc Variables
-turn = 0
-pizza = 5
-games = 2
-energy = 10
-money = 4
-ans = 0
-print("Welcome!\n\n")
-name = input ("Enter name:\n\n>>>")
-print("\n\nHello,", name)
-
 # Main functions:
 
 def mainMenu():
     print('\nType S for shop\n\nType B to build\n\nType I for inventory\n\n\n\nMoney: $', money, '\n\nEnergy:',energy, "/10\n\n\n\n\n\n\n\n")
 
+def enter_Shop():
+    global money
+    global games
+    global pizza
     
+    if(money == 0):
+        print("STOREKEEPER: If you don't have money, what good are you to me? \n\nCome back when you have something useful!!")
+    else:
+        print("STOREKEEPER: What would you like? G for video game ($ 5), P for pizza ($ 2)! \nThat's all we sell!\n\n(E to exit)")
+        ans = input("\n\n>>>")
+    if(ans.lower() == 'g' and money > 4):
+        print('\n\n',name,'paid $ 5 and recieved 1 game!')
+        money = money - 5
+        games = games + 1
+    elif(money < 5 and ans.lower() == 'g'):
+        print("SHOPKEEPER: You don' have enough money for that game!!")
+    if(ans.lower == 'p' and money > 1):
+        print('\n\n',name,'paid $ 2 and recieved one pizza!')
+        money = money -2
+        pizza = pizza + 1
+    elif(money < 2):
+        print("SHOPKEEPER: You don' have enough money for that pizza!!")
+
+def display_Inventory():
+    global games
+    global pizza
+    print('Select an energy fufiller! \n\nInventory:\n\nPizzas: p, Games: g, Exit: e')
+    print('Pizzas:',pizza)
+    print('Games:',games)
+
+def init_event_Variables():
+    #Event variables
+    global location
+    global event_list
+    global eventTiming
+    global event
+    location = "B"
+    event_list = ["were fired","were robbed", "were promoted", "won the lottery"]
+    eventTiming = int(random.uniform(3,10))
+    event = 0
+
+def init_job_Variables():    
+    #Job variables
+    global job 
+    global findJob
+    global salary
+    job = True
+    findJob = 0
+    salary = 1
+
+def init_inventory_Variables():   
+    #Inventory & Misc Variables
+    global turn
+    global pizza
+    global games
+    global energy
+    global money
+    global ans
+    turn = 0
+    pizza = 5
+    games = 2
+    energy = 10
+    money = 4
+    ans = ''
+    
+ 
+
+ 
+
+ 
+
+init_event_Variables()
+init_job_Variables()
+init_inventory_Variables()
+
+global money
+global turn
+global energy
+global location
+global ans
+
+print("Welcome!\n\n")
+name = input ("Enter name:\n\n>>>")
+print("\n\nHello,", name)
 
 while True:
     turn = turn + 1
-    
     #Main Menu
     mainMenu()
     location = input(">>>")
     #Shop
-    if(location == 'S' or location == 's'):
-        if(money == 0):
-            print("STOREKEEPER: If you don't have money, what good are you to me? \n\nCome back when you have something useful!!")
-        else:
-            print("STOREKEEPER: What would you like? G for video game ($ 5), P for pizza ($ 2)! \nThat's all we sell!\n\n(E to exit)")
-            ans = input("\n\n>>>")
-            if(ans == 'G' and money > 4):
-                print('\n\n',name,'paid $ 5 and recieved 1 game!')
-                money = money - 5
-                games = games + 1
-            elif(money < 5 and ans == 'G'):
-                print("SHOPKEEPER: You don' have enough money for that game!!")
-            
-            if(ans == 'P' and money > 1):
-                print('\n\n',name,'paid $ 2 and recieved one pizza!')
-                money = money -2
-                pizza = pizza + 1
-            elif(money < 2):
-                print("SHOPKEEPER: You don' have enough money for that pizza!!")
+   
+    if(location.lower() == 's'):
+        enter_Shop()
     if(findJob == 0):
         job = True
     else:
         findJob = findJob - 1
-    if(location == 'b' or location == 'B'):
+    if(location.lower() == 'b'):
         if(job):
             print(name, 'built a bunch of stuff and earned $', salary)
             money = money + salary
             energy = energy-1
         else:
             print("You are currently searching for a job...\n\n", findJob, "more days until you find a job!")
-    if(location == 'E' or location == 'e'):
+    if(location.lower == 'e'):
         break
-    if(location == 'I' or location == 'i'):
-        location = 'I'
-        print('Select an energy fufiller! \n\nInventory:\n\nPizzas:',pizza, '\nGames:',games, '\n\n(E to exit)')
+    if(location.lower() == 'i'):
+        display_Inventory()
         ans = input('\n\n>>>')
-        if(ans == 'P'):
+        if(ans.lower() == 'p'):
             print(name, 'ate 1 pizza and earned 2 energy!')
             pizza = pizza - 1
             energy = energy +2
-        if(ans == 'G'):
+        if(ans.lower() == 'g'):
             print(name, 'played 1 game and earned 4 energy!')
             games = games - 1
             energy = energy + 4
     if(energy > 10):
         energy = 10
-    if(ans == 'E' or  ans == 'e'):
+    if(ans.lower() == 'e'):
         print('\n\nExited inventory!')
-    elif(not location == "I"):
+    elif(not location.lower == "i"):
         energy = energy - 1
     if(eventTiming == turn):
         event = int(random.uniform(0,4))
